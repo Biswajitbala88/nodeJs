2a2f58275185485b97e71f793eadb3371139b5c5
Biswajitbala88 <biswajitbala88@gmail.com>
2023-11-28 23:36:46 +0530

    import another file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-for(i=0;i<=10;i++){
-    console.log(i);
-}
-
-const arr=[2,3,4,54];
-console.log(arr[1]);
+// for(i=0;i<=10;i++){
+//     console.log(i);
+// }
+const app = require('./app');
+// const arr=[2,3,4,54];
+console.log(app.z());
