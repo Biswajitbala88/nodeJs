13e80b67a5e9bb53a2720ab7bf2b38c4104003b5
Biswajitbala88 <biswajitbala88@gmail.com>
2023-11-28 23:41:47 +0530

    user filter in array

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // for(i=0;i<=10;i++){
 //     console.log(i);
 // }
-const app = require('./app');
-// const arr=[2,3,4,54];
-console.log(app.z());
+// const app = require('./app');
+const arr=[2,3,4,5,3,4];
+arr.filter((item)=>{
+    console.log(item);
+})
+// console.log(app.z());
