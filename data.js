642a6ff43b492d87d084dbbdeebf2ac9b6511c7e
Biswajitbala88 <biswajitbala88@gmail.com>
2023-12-04 00:50:49 +0530

    get API data external file

---
+++ data.js
@@ -0,0 +1,15 @@
+const data = [
+    {
+        name: 'Biswajit Bala',
+        email: 'biswajitbala88@gmail.com'
+    },
+    {
+        name: 'Sam',
+        email: 'sam@gmail.com'
+    },
+    {
+        name: 'Peter',
+        email: 'peter@gmail.com'
+    }
+];
+module.exports = data;
--- index.js
+++ index.js
@@ -14,12 +14,10 @@
 // console.log('hello'. yellow);
 
 const http = require('http');
+const data  = require('./data');
 http.createServer((req, resp)=>{
     resp.writeHead(200, {'Content-Type': 'application/json'});
-    resp.write(JSON.stringify({
-        name: 'Biswajit Bala',
-        email: 'biswajitbala88@gmail.com'
-    }));
+    resp.write(JSON.stringify(data));
     resp.end();
 
 }).listen(1200);
