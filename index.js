2f5cac6872f525178dc5c546de843633dcfb6069
Biswajitbala88 <biswajitbala88@gmail.com>
2023-12-06 00:26:29 +0530

    added ejs for dynamic page

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const path = require('path');
-
+const ejs = require('ejs');
 const app = express();
 const publicPath = path.join(__dirname, 'public');
-app.set('view engin', 'ejs');
+app.set('view engine', 'ejs');
 // console.log(publicPath);
 
 // app.use(express.static(publicPath));
 
 app.get('', (__, resp)=>{
     resp.sendFile(`${publicPath}/home.html`)
+});
+app.get('/profile', (__, resp)=>{
+    const data = {
+        name: 'Biswajit Bala',
+        email: 'email@gmail.com'
+    }
+    resp.render('profile',{data});
 });
 app.get('/about', (__, resp)=>{
     resp.sendFile(`${publicPath}/about.html`)
