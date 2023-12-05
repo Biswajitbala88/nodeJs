e657edb8509c63b70521dc0a1d7b7d887cc5795c
Biswajitbala88 <biswajitbala88@gmail.com>
2023-12-06 00:05:54 +0530

    remove file extension

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const publicPath = path.join(__dirname, 'public')
+const publicPath = path.join(__dirname, 'public');
+app.set('view engin', 'ejs');
 // console.log(publicPath);
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
 
+app.get('', (__, resp)=>{
+    resp.sendFile(`${publicPath}/home.html`)
+});
+app.get('/about', (__, resp)=>{
+    resp.sendFile(`${publicPath}/about.html`)
+});
+app.get('*', (__, resp)=>{
+    resp.sendFile(`${publicPath}/page_not_found.html`)
+});
 app.listen(1200);
