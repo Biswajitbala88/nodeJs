20750f1e591b28ee8a877bb54c6bcbd514429098
Biswajitbala88 <biswajitbala88@gmail.com>
2023-12-08 20:57:50 +0530

    added some comment

--- middleware.js
+++ middleware.js
@@ -10,3 +10,8 @@ module.exports = reqFilter;
 
 
 // If you were to use app.use('/myroutes', router);, for example, all the routes defined within the router instance would become accessible under the '/myroutes' path. For instance, the /profile route would then be accessed via http://yourdomain.com/myroutes/profile.
+
+// If you were to use app.use('/myroutes', router);, for example, all the routes defined within the router instance would become accessible under the '/myroutes' path. For instance, the /profile route would then be accessed via http://yourdomain.com/myroutes/profile.
+
+
+// If you were to use app.use('/myroutes', router);, for example, all the routes defined within the router instance would become accessible under the '/myroutes' path. For instance, the /profile route would then be accessed via http://yourdomain.com/myroutes/profile.
