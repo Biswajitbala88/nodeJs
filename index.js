c9f2f65eca3145a502913cdf0d2b02e2d6af8a23
Biswajitbala88 <biswajitbala88@gmail.com>
2023-12-14 23:20:42 +0530

    added some comment

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.post("/create", async (req, resp)=>{
 
 });
 
+// In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.
+
+// In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.
 
 app.listen(1200, () => {
     console.log('Server is running on port 1200');
